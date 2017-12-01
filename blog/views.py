9976087683b41bee75d818955906595feddb1b59
colonelrascals <pdhildreth@gmail.com>
2017-12-01 11:06:17 -0600

    posts view added

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
 def index(req):
-  return render(req, 'index.html', {}) 
+  posts = Post.objects.all()
+  return render(req, 'index.html', {'posts' : posts}) 
 
 def post(req):
   return HttpResponse('I am a Post')
